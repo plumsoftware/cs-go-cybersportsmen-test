175583aab84b90f04815339038938223264cf571
Slava <plumsoftwareofficial@gmail.com>
2024-09-15 18:57:18 +0600

    Архитектура - Настроил тему - Добавил шрифты - Добавил формы. - Добавил ресурсы (картинки и прочее).

---
+++ app/src/main/java/ru/plumsoftware/csgotest/presentation/ui/theme/Shapes.kt
@@ -0,0 +1,9 @@
+package ru.plumsoftware.csgotest.presentation.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+internal val Shapes = Shapes(
+    medium = RoundedCornerShape(12.dp)
+)
--- app/src/main/java/ru/plumsoftware/csgotest/start_point/MainActivity.kt
+++ app/src/main/java/ru/plumsoftware/csgotest/start_point/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ru.plumsoftware.csgotest.presentation.ui.theme.ТестНаКиберспортсменаTheme
+import ru.plumsoftware.csgotest.presentation.ui.theme.CsGoTheTestGameTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ТестНаКиберспортсменаTheme {
+            CsGoTheTestGameTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ТестНаКиберспортсменаTheme {
+    CsGoTheTestGameTheme {
         Greeting("Android")
     }
 }
